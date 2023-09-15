97a14550f413f7b951bea8e8f9b9de662d7055d2
kamighost1 <romires2000@gmail.com>
2023-09-15 23:46:33 +0300

    for lab 2

--- src/ui/components/MainPage.js
+++ src/ui/components/MainPage.js
@@ -3,8 +3,9 @@ import styles from '../css/index.module.css'
 import Navbar from '../elements/navbar.js';
 import Button from '../elements/button';
 import { LocalStorage } from '../Utils';
+import { AccountClass } from '../../Account';
 
-
+global.Account = AccountClass
 
 export default function MainPage(props) {
 
@@ -14,6 +15,7 @@ export default function MainPage(props) {
             <Navbar />
             
             <div className={styles.content}>
+                <Button text="Account" className={styles.full} onClick={()=>{props.setAccountPage(true)}}/>
                 <Button text="Code" className={styles.full} onClick={()=>{props.setCode(true)}}/>
             </div>
         </div>
